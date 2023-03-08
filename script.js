fb0da2739956b22c71f754b0dcc7d15695936ee1
Eric Wang <ericw029@gmail.com>
2023-03-08 18:47:21 -0500

    Deleted tutorial framework, added draggable cards, flipping, and drag area restrictions

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
+var activeImage = "";
+var zIndex = 0;
 
+// Drag Functions
 
-function drag_over(event) {
-	event.preventDefault();
-	return false;
+// you can drag the Card class, supports draging img, and restricts dragging to the visible window without scrolling
+$( ".Card" ).draggable({
+    containment: [0, 0, $(document).width()-100, $(document).height()-140],
+    scroll: false
+});
+
+$(document).keyup(function(e) {
+    // f key
+    if (e.keyCode == 70 && activeImage != "") {
+      flip(document.getElementById(activeImage));
+    }
+});
+
+//monitors object for hover, hover and hover off functions
+$('.Card').hover(function(){
+   activeImage = this.id;
+   this.style.border = "solid 3px orange";
+  console.log("ddddd");
+   
+}, function () {
+   activeImage = ""; // not active
+   this.style.border = "solid 3px green";
+  console.log("eeeee");
+});
+
+// click card to send to front
+$('.Card').mousedown(function(){
+  zIndex++;  
+  $(this).css('zIndex', zIndex);
+});
+
+// flip card by pressing f
+function flip(obj) {
+  console.log(obj.src);
+  if(obj.src != "https://pile-drag.ericwang3.repl.co/cards/Reverse.png") {
+    obj.src = "https://pile-drag.ericwang3.repl.co/cards/Reverse.png";
+    console.log(obj.src);
+  }
+  else {
+    obj.src = "https://pile-drag.ericwang3.repl.co/cards/" + obj.id + ".png";
+    console.log(obj.src);
+  }
 }
-
-function drag_start(event) {
-  this.style.opacity = '0.4';
-  var style = window.getComputedStyle(event.target, null);
-  event.dataTransfer.effectAllowed = 'move';
-  event.dataTransfer.setData("text/plain", (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY) + ',' + event.target.getAttribute('data-item'));
-}
-
-function drag_end(event) {
-  this.style.opacity = '0.5';
-}
-
-function drop(event) {
-  event.stopPropagation();
-  var offset = event.dataTransfer.getData("text/plain").split(',');
-  let allcards = document.getElementsByClassName('card');
-  console.log(offset);
-  allcards[parseInt(offset[2])].style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
-  allcards[parseInt(offset[2])].style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
-  event.preventDefault();
-  return false;
-}
-
-let cards= document.querySelectorAll('.card');
-cards.forEach(function (card) {
-card.addEventListener('dragstart', drag_start);
-card.addEventListener('dragend', drag_end); document.body.addEventListener('dragover', drag_over, false); document.body.addEventListener('drop', drop, false); });
 
